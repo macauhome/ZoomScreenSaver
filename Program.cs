cb05a405a4d3800d875b6259b6b02d6959a7e1d3
macauhome <macauhome@gmail.com>
2025-01-09 16:41:20 +0800

    increase image display duration to 15 seconds

--- Program.cs
+++ Program.cs
@@ -19,13 +19,14 @@ namespace ZoomScreenSaver
         private PointF currentPosition;
         private float currentZoom;
         private float panSpeedX, panSpeedY;
+         private float panDirectionX, panDirectionY; // Add variables to control the direction of panning
         private Point lastMousePosition;
         //private PictureBox pictureBox;
         private Image? currentImage;
         private Stopwatch? stopwatch;
-        private readonly float zoomInLimit = 1.2f; //1.5f;// Maximum zoom limit
+        private readonly float zoomInLimit = 1.1f; //1.5f;// Maximum zoom limit
         private readonly float zoomOutLimit = 1.0f; //1.0f; // Minimum zoom limit
-        private int imageDisplayDuration = 5000; // Display duration for each image in milliseconds
+        private int imageDisplayDuration = 15000; // Display duration for each image in milliseconds
         private float fadeStep = 0.05f; // Opacity change per step
         private bool fadingOut = false;
         private System.Windows.Forms.Timer fadeTimer;
@@ -35,28 +36,30 @@ namespace ZoomScreenSaver
             imageFiles = Array.Empty<string>();
             currentImage=null;
             photoPath = string.Empty;
-            zoomSpeed = 0.001f;
+
+            zoomSpeed = 0.01f;
             currentZoom = 1.0f; // Start at normal size
             currentPosition = new PointF(0, 0); // Reset panning position
             panSpeedX = 0.001f; // Set pan speed in the X direction (adjust as needed)
             panSpeedY = 0.001f; // Set pan speed in the Y direction (adjust as needed)
 
-            // Initialize fade timer
-            fadeTimer = new System.Windows.Forms.Timer();
-            fadeTimer.Interval = 50; // Adjust fade speed
-            fadeTimer.Tick += FadeTimer_Tick;
-
             LoadSettings();
             InitializeComponents();
             LoadImages();
             ResetAnimation();
+            RandomizePosition();
             StartRenderLoop();
+
+            // Initialize fade timer
+            fadeTimer = new System.Windows.Forms.Timer();
+            fadeTimer.Interval = 50; // Adjust fade speed
+            fadeTimer.Tick += FadeTimer_Tick;            
         }
 
         private void LoadSettings()
         {
             photoPath = ConfigurationManager.AppSettings["PhotoPath"] ?? "D:\\ScreenSaver";
-            zoomSpeed =float.Parse(ConfigurationManager.AppSettings["ZoomSpeed"] ?? "0.001");
+            zoomSpeed =float.Parse(ConfigurationManager.AppSettings["ZoomSpeed"] ?? "0.01");
             
         }
 
@@ -65,12 +68,15 @@ namespace ZoomScreenSaver
             this.WindowState = FormWindowState.Maximized;
             this.WindowState = FormWindowState.Normal;
             this.FormBorderStyle = FormBorderStyle.None;
-            this.TopMost = true;
-            this.Size = new Size(1920, 1080);
+            //this.TopMost = true;
+            if (Screen.PrimaryScreen != null)
+            {
+                this.Size = Screen.PrimaryScreen.Bounds.Size;
+            }
             this.BackColor = Color.Black;
             this.DoubleBuffered = true;
             this.KeyDown += (s, e) => { if (e.KeyCode == Keys.Escape) Application.Exit(); };
-            this.MouseMove += MainForm_MouseMove;
+            //this.MouseMove += MainForm_MouseMove;
             this.MouseClick += (s, e) => Application.Exit();
             this.KeyPress += (s, e) => Application.Exit();
             Cursor.Hide();  // Hide the mouse cursor
@@ -101,9 +107,6 @@ namespace ZoomScreenSaver
                 imageFiles = new string[0];
             }
         }
-
-
-
         private void DisplayImage(string imagePath)
         {
             currentImage = Image.FromFile(imagePath);
@@ -196,7 +199,6 @@ namespace ZoomScreenSaver
             else // Fade in
             {
                 this.Opacity += fadeStep; // Increase opacity
-
                 if (this.Opacity >= 1) // 
                 {
                     fadeTimer.Stop();
@@ -212,7 +214,6 @@ namespace ZoomScreenSaver
             fadeTimer.Start(); // Start fade timer for fading in
         }
 
-
         private void SwitchToNextImage()
         {
             currentImage?.Dispose();
@@ -221,9 +222,10 @@ namespace ZoomScreenSaver
             currentImageIndex = (currentImageIndex + 1) % imageFiles.Length;
             currentImage = Image.FromFile(imageFiles[currentImageIndex]);
             
+            ResetAnimation();
             // Randomly set initial position
             RandomizePosition();
-            ResetAnimation();
+            
         }
         private void RandomizePosition()
         {
@@ -235,6 +237,14 @@ namespace ZoomScreenSaver
 
             currentPosition.X = (float)(random.NextDouble() * Math.Max(0, maxPosX));
             currentPosition.Y = (float)(random.NextDouble() * Math.Max(0, maxPosY));
+
+            // Randomize the pan direction
+            panSpeedX = (float)(random.NextDouble() * 2 - 1) * 0.01f; // Random speed between -0.5 and 0.5
+            panSpeedY = (float)(random.NextDouble() * 2 - 1) * 0.01f; // Random speed between -0.5 and 0.5
+
+            zoomSpeed = GetRandomBoolean() ? -Math.Abs(zoomSpeed) : Math.Abs(zoomSpeed); // Randomize the zoom direction
+            panSpeedX = GetRandomBoolean() ? -Math.Abs(panSpeedX) : Math.Abs(panSpeedX); // Randomize the pan direction	
+            panSpeedY = GetRandomBoolean() ? -Math.Abs(panSpeedY) : Math.Abs(panSpeedY); // Randomize the pan direction
         }
 
         private void ResetAnimation()
@@ -243,7 +253,7 @@ namespace ZoomScreenSaver
             zoomSpeed = 0.01f; // Adjust zoom speed
             panSpeedX = 0.01f; // Set pan speed in X direction (adjustable)
             panSpeedY = 0.01f; // Set pan speed in Y direction (adjustable)
-            imageDisplayDuration = 5000; // Reset display time for new image
+            imageDisplayDuration = 15000; // Reset display time for new image
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -284,8 +294,11 @@ namespace ZoomScreenSaver
                 array[j] = temp;
             }
         }
-
-
+        
+        private bool GetRandomBoolean()
+        {
+            return random.Next(2) == 0; // Generates 0 or 1, returns true if 0, false if 1
+        }
     }
 
     public class SettingsForm : Form
@@ -337,11 +350,49 @@ namespace ZoomScreenSaver
 static class Program
     {
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ScreenSaverForm());
+        if (args.Length > 0)
+        {
+            string firstArgument = args[0].ToLower().Trim();
+            string? secondArgument=null;
+            
+            // Handle cases where arguments are separated by colon.
+            // Examples: /c:1234567 or /P:1234567
+            if (firstArgument.Length > 2)
+            {
+                secondArgument = firstArgument.Substring(3).Trim();
+                firstArgument = firstArgument.Substring(0, 2);
+            }
+            else if (args.Length > 1)
+                secondArgument = args[1];
+    
+            if (firstArgument == "/c")           // Configuration mode
+            {
+                Application.Run(new SettingsForm());
+            
+            }
+            else if (firstArgument == "/p")      // Preview mode
+            {
+                Application.Run(new ScreenSaverForm());
+            }
+            else if (firstArgument == "/s")      // Full-screen mode
+            {
+                Application.Run(new ScreenSaverForm());
+            } 
+            else    // Undefined argument
+            {
+                MessageBox.Show("Sorry, but the command line argument \"" + firstArgument +
+                    "\" is not valid.", "ScreenSaver",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            }
+            else    // No arguments - treat like /c
+            {
+                Application.Run(new ScreenSaverForm());
+            }                 
         }
     }
 }
